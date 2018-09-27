7ac636c2aff51f582435bbeaa68c94e8348136f8
dumblole <move2234@gmail.com>
2018-09-27 18:21:32 -0400

    More organization and added a contact and a skills page for routing

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>About </h1>
+    <p>
+      We're the only site running on your computer dedicated to showing the best photos and videos
+      of pandas eating lots of food.
+    </p>
+  </Layout>
+);
---
+++ src/pages/skills.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>About </h1>
+    <p>
+      We're the only site running on your computer dedicated to showing the best photos and videos
+      of pandas eating lots of food.
+    </p>
+  </Layout>
+);
