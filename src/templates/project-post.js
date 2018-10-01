6a708abcf6405aa82ee3378cf2e107739264db1a
dumblole <move2234@gmail.com>
2018-09-30 23:24:11 -0400

    Added styling for project markdown pages

--- src/templates/project-post.js
+++ src/templates/project-post.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
-
+import "../styles/components/_blog-project-post.scss";
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} className={"blog"} />
         <p>SLOT</p>
       </div>
     </Layout>
