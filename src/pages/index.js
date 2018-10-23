5f95457db6931e3866b71c9dc7dc307b9a0d4abe
dumblole <move2234@gmail.com>
2018-10-23 06:33:54 -0400

    sync

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,34 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Img from "gatsby-image";
+import "normalize.css";
+import "../styles/styles.scss";
 
 export default ({ data }) => {
   return (
-    <Layout>
-      <h1>aaaa</h1>
-      <Img fluid={data.imageTwo.childImageSharp.fluid} />
-      <h1>Thanks for stopping by!</h1>
-    </Layout>
+    <div className="home-back">
+      <h1 className="home-back">aaaa</h1>
+      <p>abcdefghijklmnopqrstuvwxyz</p>
+      <h1 className="home-back">Thanks for stopping by!</h1>
+    </div>
   );
 };
 
-export const ImageQuery = graphql`
-  query {
-    imageOne: file(relativePath: { eq: "1.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    imageTwo: file(relativePath: { eq: "abc.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1080) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`;
+// export const ImageQuery = graphql`
+//   query {
+//     imageOne: file(relativePath: { eq: "1.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+//     imageTwo: file(relativePath: { eq: "abc.jpg" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1080) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+//   }
+// `;
