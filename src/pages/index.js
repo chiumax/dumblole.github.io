2e525f6ccaf81daa925c4c5346406b8ab4e809ed
dumblole <dumblole@gmail.com>
2018-10-24 17:18:31 -0400

    sync

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,19 @@ import Img from "gatsby-image";
 import "normalize.css";
 import "../styles/styles.scss";
 
-export default ({ data }) => {
+export default class Home extends React.Component{
+	render(){
+		const lorem ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla at volutpat diam ut venenatis tellus in metus. Suscipit adipiscing bibendum est ultricies integer quis auctor elit. Mattis pellentesque id nibh tortor id aliquet lectus. Nulla facilisi cras fermentum odio eu feugiat. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Non blandit massa enim nec dui nunc mattis. Ac felis donec et odio pellentesque. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas. Cursus in hac habitasse platea dictumst. Lobortis elementum nibh tellus molestie nunc non. Sit amet tellus cras adipiscing enim. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Congue eu consequat ac felis donec.";
   return (
     <div className=" home-back">
       <h1 className="title-home">
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        DUMBLOLE
       </h1>
-      <h1 className="title-home">Thanks for stopping by!</h1>
+      <h1 className="title-home">
+      	{(Math.random()*701)+}PLACEHOLDERTEXTPLACEHOLDERTEXT
+      </h1>
     </div>
-  );
+  )}
 };
 
 // export const ImageQuery = graphql`
