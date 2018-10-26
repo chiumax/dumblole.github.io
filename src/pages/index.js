b3715b39931c3b36ef77b16e0eeee87254128813
dumblole <dumblole@gmail.com>
2018-10-26 17:44:32 -0400

    sync

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,12 @@ import "../styles/styles.scss";
 export default class Home extends React.Component {
   render() {
     const intro = ["Just Somebody", "Trying To Figure", "The World Out."];
+    const introLink = [
+    {link: "/about/", name: "about"},
+    {link: "/skills/", name: "skills"},
+    {link: "/project/", name: "projects"},
+    {link: "/blog/", name: "blogs"},
+    {link: "/contact/", name: "contact"}]
     const lorem = [
       "Sed ut volutpat felis. Etiam euismod sit amet ante ut ornare. Donec scelerisque, purus in laoret sodales, orci ipsum porttitor erat, et placerat metus arcu eget turpis. Integer sodales a quam a porta. Nunc et risus efficitur odio elementum venenatis et eu nisi. Sed consequat quis lorem nec finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas eu aliquam nibh. In hac habitasse platea dictumst. Suspendisse convallis, mi mattis hendrerit aliquam, diam est suscipit est, eget iaculis nisi sapien volutpat lectus. Fusce et consequat sem. Donec non sollicitudin massa, quis congue lorem. Aliquam sollicitudin eleifend ipsum, at gravida massa scelerisque vel. Donec id ante est. Pellentesque bibendum lobortis orci, eu aliquet mauris dignissim ut. Suspendisse ut dui quis odio rutrum maximus at ac mauris. Morbi tristique interdum eros id rhoncus. Duis aliquet consectetur ipsum, quis placerat eros porttitor sed. Curabitur magna orci, faucibus id sed.",
       "Nam fermentum sed metus a semper. Maecenas consectetur ex pretium urna tempor porttitor. Nulla id elit sed quam placerat maximus ut eget elit. Suspendisse tincidunt venenatis velit sed elementum. Nulla iaculis nisl rutrum nulla mattis porttitor. Mauris eget mauris pulvinar leo mollis tincidunt sed at justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla tempus, sapien ut sagittis pharetra, felis tortor pellentesque lorem, in tempor neque odio ac velit. Nam tempus eleifend lorem non scelerisque. Proin dapibus tortor elit, non ornare massa feugiat quis. Nullam in ipsum in risus blandit vulputate quis eu mauris. Phasellus ac leo scelerisque, vehicula felis ut, maximus risus. Pellentesque sed risus semper ipsum venenatis venenatis. Proin tortor lorem, mollis at pellentesque a, feugiat eu lorem. Phasellus volutpat erat libero, sed fermentum sapien aliquet vel. Curabitur tempor erat quis eros viverra, vitae sagittis urna accumsan. Quisque metus.",
@@ -17,20 +23,25 @@ export default class Home extends React.Component {
     return (
       <div className=" home-back">
         <h1 className="title-home">DUMBLOLE</h1>
-
+        <div className="desc-wrap" key={intro}>
+        <h1 className="lorem">{lorem[Math.floor(Math.random() * lorem.length)]}</h1>
+        </div>
         {intro.map(intro => {
           return (
             <div className="desc-wrap" key={intro}>
-              <h1 className="lorem">{lorem[Math.floor(Math.random() * lorem.length + 1)]}</h1>
+              <h1 className="lorem">{lorem[Math.floor(Math.random() * lorem.length)]}</h1>
               <h1>
                 &nbsp;
                 {intro}
                 &nbsp;
               </h1>
-              <h1 className="lorem">{lorem[Math.floor(Math.random() * lorem.length + 1)]}</h1>
+              <h1 className="lorem">{lorem[Math.floor(Math.random() * lorem.length)]}</h1>
             </div>
           );
         })}
+        <div className="desc-wrap" key={intro}>
+        <h1 className="lorem">{lorem[Math.floor(Math.random() * lorem.length)]}</h1>
+        </div>
       </div>
     );
   }
