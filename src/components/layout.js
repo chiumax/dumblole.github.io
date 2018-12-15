d1f93134c31f93a682c7b66e55fb6eb55593a56d
dumblole <move2234@gmail.com>
2018-12-15 11:50:46 -0500

    add header

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,14 +20,89 @@ export default ({ children }) => (
       <div className="">
         <div className="">
           <div className="headerLinkWrap">
-            <div>
+            {/* <div>
               <Link className="linke" to={`/`}>
                 DUMBLOLE
               </Link>
-            </div>
+            </div> */}
 
-            <div>
-              <Link className="link" to={`/project/`}>
+            <Link
+              class="Menu-list-item"
+              activeClassName="ActiveMenu Menu-list-item"
+              data-offset="20"
+              to={`/`}
+            >
+              Home
+              <span class="Mask Laks">
+                <span>Home</span>
+              </span>
+              <span class="Mask">
+                <span>Home</span>
+              </span>
+            </Link>
+            <Link
+              class="Menu-list-item"
+              div
+              activeClassName="ActiveMenu Menu-list-item"
+              data-offset="16"
+              to={`/about/`}
+            >
+              About
+              <span class="Mask Laks">
+                <span>About</span>
+              </span>
+              <span class="Mask">
+                <span>About</span>
+              </span>
+            </Link>
+            <Link
+              class="Menu-list-item"
+              div
+              activeClassName="ActiveMenu Menu-list-item"
+              data-offset="12"
+              to={`/project/`}
+            >
+              Work
+              <span class="Mask Laks">
+                <span>Work</span>
+              </span>
+              <span class="Mask">
+                <span>Work</span>
+              </span>
+            </Link>
+            <Link
+              class="Menu-list-item"
+              div
+              activeClassName="ActiveMenu Menu-list-item"
+              data-offset="12"
+              to={`/blog/`}
+            >
+              Words
+              <span class="Mask Laks">
+                <span>Words</span>
+              </span>
+              <span class="Mask">
+                <span>Words</span>
+              </span>
+            </Link>
+
+            <Link
+              class="Menu-list-item"
+              div
+              activeClassName="ActiveMenu Menu-list-item"
+              data-offset="8"
+              to={`/contact/`}
+            >
+              Contact
+              <span class="Mask Laks">
+                <span>Contact</span>
+              </span>
+              <span class="Mask">
+                <span>Contact</span>
+              </span>
+            </Link>
+
+            {/* <Link className="link" to={`/project/`}>
                 Projects
               </Link>
               <Link className="link" to={`/blog/`}>
@@ -38,8 +113,7 @@ export default ({ children }) => (
               </Link>
               <Link className="link" to={`/about/`}>
                 About
-              </Link>
-            </div>
+              </Link> */}
           </div>
           <div className={""}>
             <div className=" ">{children}</div>
@@ -49,106 +123,3 @@ export default ({ children }) => (
     )}
   />
 );
-// // === Variables =====
-// $perspective:     60rem;
-// $font-size:       4.25rem;
-// $split-position:  49%;
-// $split-thickness: 4px;
-// $split-color:     #FF2C75;
-
-// // === Settings =====
-// %font-settings {
-//   font-family: "Gilroy ExtraBold", system-ui, sans-serif;
-//   font-style: normal;
-//   font-weight: normal;
-//   -webkit-font-smoothing: antialiased;
-//   -webkit-font-kerning: normal;
-//   -webkit-text-size-adjust: 100%;
-// }
-
-// // === Codepen Setup =====
-// html,
-// body {
-//   width: 100vw;
-//   height: 100vh;
-// }
-// body {
-//   @extend %font-settings;
-//   background: linear-gradient(45deg, #02001F,#1F1B4E);
-//   transform-style: preserve-3d;
-//   transform: perspective($perspective);
-//   position: fixed;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center; 
-// }
-
-// // === Menu <ul> =====
-// .Menu-list {
-//   font-size: $font-size;
-//   line-height: 1.2;
-//   text-transform: uppercase;
-//   text-align: center;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   transform: rotateX(-10deg) rotateY(20deg); // overwritten by JS
-// }
-
-// // === Menu item =====
-// .Menu-list-item {
-//   position: relative;
-//   color: transparent;
-//   cursor: pointer;
-  
-//   // === Split Line =====
-//   &::before {
-//     content: '';
-//     display: block;
-//     position: absolute;
-//     top: $split-position;
-//     left: -10%;
-//     right: -10%;
-//     height: $split-thickness;
-//     border-radius: $split-thickness;
-//     margin-top: -($split-thickness / 2);
-//     background: $split-color;
-//     transform: scale(0);
-//     transition: transform .8s cubic-bezier(.16,1.08,.38,.98);
-//     z-index: 1;
-//   }
-// }
-
-// // === Top/bottom mask =====
-// .Mask {
-//   display: block;
-//   position: absolute;
-//   overflow: hidden;
-//   color: $split-color;
-//  background: green;
-//   height: 170%;
-//   transition: all .8s cubic-bezier(.16,1.08,.38,.98);
-//   transform: rotate(-45deg);
-//   span { display: block; }
-// }
-// .Laks{
-//   position: absolute;
-//   top:0;
-//    transform: translateX(-110px) rotate(-45deg) ;
-//   span{transform: rotate(45deg) translateX(110px);}
-// }
-// // === Bottom mask specific =====
-// .Mask + .Mask {
-//  bottom: -119%;
-//  transform: translateX(10px) translateY(15px) rotate(-45deg) ;
-  
-//   span { transform:  rotate(45deg) translateY(-$split-position) translateX(-10px); }
-// }
-
-// // === Where the fun stuff happens =====
-// .Menu-list-item:hover,
-// .Menu-list-item:active {
-//   .Mask { color: #FFF; transform: skewX(12deg) translateX(5px); }
-//   .Mask + .Mask { transform: skewX(12deg) translateX(-5px); }
-//   &::before { transform: scale(1); }
-// }
