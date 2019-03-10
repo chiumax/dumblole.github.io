4ed9d1883523696e901f72d3d0979781ba9c82b2
dumblole <move2234@gmail.com>
2019-03-10 15:56:07 -0400

    captcha

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "../components/firebase";
 import firebase from "firebase";
+import anime from "animejs";
 import Layout from "../components/layout";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default class Contact extends React.Component {
   state = {
@@ -9,11 +11,16 @@ export default class Contact extends React.Component {
     email: "",
     subject: "",
     message: "",
-    status: ""
+    status: "contactFailure",
+    submitValid: true,
+    captcha: false
   };
+  captchaRef = React.createRef();
   submitContactForm = e => {
     e.preventDefault();
+    this.animateAlertOut("data-alert");
     let sendMail = firebase.functions().httpsCallable("sendMail");
+
     sendMail({
       name: this.state.name,
       email: this.state.email,
@@ -21,25 +28,38 @@ export default class Contact extends React.Component {
       message: this.state.message
     })
       .then(result => {
-        console.log(JSON.stringify(result));
         if (result.data.response === true) {
-          this.setState(prevState => ({
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-            status: "contactSuccess"
-          }));
-        } else {
-          this.setState(prevState => ({
-            status: "contactFailure"
-          }));
+          this.setState(
+            prevState => ({
+              name: "",
+              email: "",
+              subject: "",
+              message: "",
+              status: "contactSuccess",
+              submitValid: true,
+              captcha: false
+            }),
+            () => {
+              this.captchaRef.reset();
+              this.animateAlertIn("data-alert");
+            }
+          );
+        } else if (result.data.response === false) {
+          this.setState(
+            prevState => ({
+              status: "contactFailure"
+            }),
+            () => this.animateAlertIn("data-alert")
+          );
         }
       })
       .catch(error => {
-        this.setState(prevState => ({
-          status: "contactFailure"
-        }));
+        this.setState(
+          prevState => ({
+            status: "contactFailure"
+          }),
+          () => this.animateAlertIn("data-alert")
+        );
         console.log("nodex");
       });
     console.log(
@@ -51,25 +71,76 @@ export default class Contact extends React.Component {
 
   handleContactFormChange = e => {
     e.persist();
-    this.setState(prevState => ({ [e.target.name]: e.target.value }));
+
+    this.setState(
+      prevState => ({ [e.target.name]: e.target.value }),
+      () => {
+        let submitValid =
+          this.state.captcha &&
+          this.state.email &&
+          this.state.name &&
+          this.state.subject &&
+          this.state.message;
+        this.setState(prevState => ({ submitValid: !submitValid }));
+      }
+    );
   };
   renderAlert = () => {
     if (this.state.status === "contactSuccess") {
-      return <div className={this.state.status}>Success! Thanks for contacting me.</div>;
-    } else if (this.state.status === "contactFailure") {
       return (
-        <div className={this.state.status}>
+        <div className={this.state.status} data-alert>
+          Success! Thanks for contacting me.
+        </div>
+      );
+    } else {
+      return (
+        <div className={this.state.status} data-alert>
           Oops! Something went wrong... Please try again and see if it works. If not, feel free to
           directly contact me at{" "}
-          <a target="_blank" href={"mailto:dumblole@gmail.com"}>
+          <a className={"inline-link"} target="_blank" href={"mailto:dumblole@gmail.com"}>
             dumblole@gmail.com
           </a>
           .
         </div>
       );
-    } else {
-      return;
     }
+  };
+  animateAlertIn = el => {
+    anime({
+      targets: `[${el}]`,
+      translateY: 0,
+      duration: 500,
+      easing: "easeInOutSine"
+    });
+  };
+  animateAlertOut = el => {
+    anime({
+      targets: `[${el}]`,
+      translateY: -30,
+      duration: 500,
+      easing: "easeInOutSine",
+      complete: () => {
+        anime({
+          targets: `[${el}]`,
+          opacity: 1,
+          duration: 0
+        });
+      }
+    });
+  };
+  callCaptcha = e => {
+    this.setState(
+      prevState => ({ captcha: true }),
+      () => {
+        let submitValid =
+          this.state.captcha &&
+          this.state.email &&
+          this.state.name &&
+          this.state.subject &&
+          this.state.message;
+        this.setState(prevState => ({ submitValid: !submitValid }));
+      }
+    );
   };
   render() {
     return (
@@ -77,6 +148,7 @@ export default class Contact extends React.Component {
         <div className={"contactContainer"} key="contact">
           <div className={"contactHeader"}>Say hello.</div>
           {this.renderAlert()}
+
           <form
             className={"contactForm"}
             onSubmit={e => {
@@ -91,7 +163,6 @@ export default class Contact extends React.Component {
               value={this.state.name}
               type="text"
               name="name"
-              required
             />
             Email address
             <input
@@ -101,7 +172,6 @@ export default class Contact extends React.Component {
               value={this.state.email}
               type="email"
               name="email"
-              required
             />
             Subject
             <input
@@ -111,7 +181,6 @@ export default class Contact extends React.Component {
               value={this.state.subject}
               type="text"
               name="subject"
-              required
             />
             Message
             <textarea
@@ -122,9 +191,16 @@ export default class Contact extends React.Component {
               type="text"
               name="message"
               rows={5}
-              required
             />
-            <input type="submit" value="Send" />
+            <ReCAPTCHA
+              ref={e => (this.captchaRef = e)}
+              className={"contactCaptcha"}
+              onChange={e => {
+                this.callCaptcha(e);
+              }}
+              sitekey="6Ld8tJYUAAAAAOKxJFRJDNOdI_tbzVhPNOEBq8PC"
+            />
+            <input type="submit" value="Send" disabled={this.state.submitValid} />
           </form>
         </div>
       </Layout>
