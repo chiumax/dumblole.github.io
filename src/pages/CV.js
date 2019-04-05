d3e052cde4757add8db5f04777c69469c585b3c6
dumblole <dumblole@gmail.com>
2019-04-05 19:25:30 -0400

    finished querying data for cv just need to style it and fill it with content

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,11 +1,36 @@
 import React from "react";
 
 import Layout from "../components/layout";
+const json = require("./cv/cv.json");
 
 export default ({ data }) => {
   return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html }} />
+    <div className={"cvWrap"} >
+    {console.log(json)}
+
+      {json.data.map((item)=>{
+        return(<div key={item.name} className={"cvSection"}><div className={"cvName"}>{item.name}</div><div className={"cvItemsContainer"}>{item.items.map((entry)=>{
+          return(<div className={"cvItems"}>
+          <div className={"cvColumns"}>
+          <div className={"cvTitle"}>
+          {entry.title.map((text)=>{
+            return(<div>{text}</div>)
+          })}
+          </div>
+          <div className={"cvDate"}>
+          {entry.date.map((text)=>{
+            return(<div>{text}</div>)
+          })}
+          </div>
+          </div>
+          <div className={"cvDetails"}>
+          {entry.details.map((text)=>{
+            return(<div>{text}</div>)
+          })}
+          </div>
+          </div>)
+        })}</div></div>)
+      })}
       {console.log(data.allMarkdownRemark.edges[0])}
     </div>
   );
