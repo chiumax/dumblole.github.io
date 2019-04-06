b7b47b5b5003127548956dec5737184bafe34651
dumblole <move2234@gmail.com>
2019-04-06 08:25:11 -0400

    sync

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -5,31 +5,40 @@ const json = require("./cv/cv.json");
 
 export default ({ data }) => {
   return (
-    <div className={"cvWrap"} >
-    {console.log(json)}
+    <div className={"cvWrap"}>
+      {console.log(json)}
 
-      {json.data.map((item)=>{
-        return(<div key={item.name} className={"cvSection"}><div className={"cvName"}>{item.name}</div><div className={"cvItemsContainer"}>{item.items.map((entry)=>{
-          return(<div className={"cvItems"}>
-          <div className={"cvColumns"}>
-          <div className={"cvTitle"}>
-          {entry.title.map((text)=>{
-            return(<div>{text}</div>)
-          })}
+      {json.data.map(item => {
+        return (
+          <div key={item.name} className={"cvSection"}>
+            <div className={"cvName"}>{item.name}</div>
+            <div className={"cvItemsContainer"}>
+              {item.items.map(entry => {
+                return (
+                  <div className={"cvItems"}>
+                    <div className={"cvColumns"}>
+                      <div className={"cvTitle"}>
+                        {entry.title.map(text => {
+                          return <div>{text}</div>;
+                        })}
+                      </div>
+                      <div className={"cvDate"}>
+                        {entry.date.map(text => {
+                          return <div>{text}</div>;
+                        })}
+                      </div>
+                    </div>
+                    <div className={"cvDetails"}>
+                      {entry.details.map(text => {
+                        return <div>{text}</div>;
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className={"cvDate"}>
-          {entry.date.map((text)=>{
-            return(<div>{text}</div>)
-          })}
-          </div>
-          </div>
-          <div className={"cvDetails"}>
-          {entry.details.map((text)=>{
-            return(<div>{text}</div>)
-          })}
-          </div>
-          </div>)
-        })}</div></div>)
+        );
       })}
       {console.log(data.allMarkdownRemark.edges[0])}
     </div>
