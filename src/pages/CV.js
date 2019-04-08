05ba9f1d41debd5ebcdff694dad98597220a98b2
dumblole <move2234@gmail.com>
2019-04-08 06:31:15 -0400

    finished styling cv page. Just need to do content and then finish up icon animation and then i think i'll be done

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -5,37 +5,44 @@ const json = require("./cv/cv.json");
 
 export default ({ data }) => {
   return (
-    <div className={"cvWrap"}>
+    <div className={"cvWrap content-container"}>
       {console.log(json)}
 
       {json.data.map(item => {
         return (
-          <div key={item.name} className={"cvSection"}>
-            <div className={"cvName"}>{item.name}</div>
-            <div className={"cvItemsContainer"}>
-              {item.items.map(entry => {
-                return (
-                  <div className={"cvItems"}>
-                    <div className={"cvColumns"}>
-                      <div className={"cvTitle"}>
-                        {entry.title.map(text => {
-                          return <div>{text}</div>;
-                        })}
+          <div key={item.name} className={"cvSectionWrap"}>
+            <div className={"cvSection"}>
+              <div className={"cvColumn"}>
+                <div className={"cvName"}>{item.name}</div>
+              </div>
+              <div className={"cvColumn"}>
+                <div className={"cvItemsContainer"}>
+                  {item.items.map(entry => {
+                    return (
+                      <div className={"cvItems"}>
+                        <div className={"cvColumns"}>
+                          <div className={"cvTitle"}>
+                            {entry.title.map(text => {
+                              return <div>{text}</div>;
+                            })}
+                          </div>
+                          <div className={"cvDate"}>
+                            {entry.date.map(text => {
+                              return <div>{text}</div>;
+                            })}
+                          </div>
+                        </div>
+                        <div className={"cvDetails"}>
+                          {entry.details.map(text => {
+                            return <div>{text}</div>;
+                          })}
+                        </div>
                       </div>
-                      <div className={"cvDate"}>
-                        {entry.date.map(text => {
-                          return <div>{text}</div>;
-                        })}
-                      </div>
-                    </div>
-                    <div className={"cvDetails"}>
-                      {entry.details.map(text => {
-                        return <div>{text}</div>;
-                      })}
-                    </div>
-                  </div>
-                );
-              })}
+                    );
+                  })}
+                </div>
+              </div>
+              {/* <div className={"cvColumn"} /> */}
             </div>
           </div>
         );
