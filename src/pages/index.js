62d141d8332205b9602e2ae4e94806b050de08a9
dumblole <move2234@gmail.com>
2019-06-25 22:34:52 -0400

    minor fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -207,11 +207,10 @@ export default class About extends React.Component {
       }));
       anime({
         targets: `[${el}]`,
-        translateY: [200, 0],
+
         opacity: [0, 1],
-        duration: 1000,
-        easing: "easeInOutSine",
-        delay: anime.stagger(300)
+        duration: 800,
+        easing: "linear"
       });
     }
   };
